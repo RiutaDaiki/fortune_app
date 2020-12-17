f9beac8c073dd6ff9e14be69063743cd8f6b01aa
riuta <daikiriuta0926@gmail.com>
2020-12-17 22:26:59 +0900

    top

--- app/src/main/java/com/example/yourfourtune/MainActivity.kt
+++ app/src/main/java/com/example/yourfourtune/MainActivity.kt
@@ -12,46 +12,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        fun getNumber() {
-//
-//            val firstText:String = firstNumber.toString()
-//            val secondText = secondNumber.toString()
-//
-//
-//
-//            val map1 = mapOf("0" to "なかなか", "1" to "まずまず", "2" to "とても", "3" to "めっちゃ", "4" to "相当","5" to "とんでもなく", "6" to "SUPER","7" to "DX", "8" to "お前だけは", "9" to "おそらく")
-//            val map2 = mapOf("0" to "大吉", "1" to "庄吉", "2" to "小吉", "3" to "茂吉", "4" to "中吉","5" to "大凶", "6" to "たぬきち","7" to "こっちみんな", "8" to "無吉", "9" to "ナイス！")
-//
-//            val makeResult = map1[firstText] + map2[secondText]
-//
-//            println(firstText)
-//
-//            fortuneBtn.setOnClickListener {
-//
-//                resultText.text = makeResult
-//
-//
-//
-//            }
-//
-//        }
-//        getNumber()
 
 
+        fun getNumber() {
 
-        fortuneBtn.setOnClickListener {
-            val firstText:String = firstNumber.toString()
-            val secondText = secondNumber.toString()
+            val firstText:String = firstNumber.text.toString()
+            val secondText = secondNumber.text.toString()
 
 
-            println(firstText)
 
             val map1 = mapOf("0" to "なかなか", "1" to "まずまず", "2" to "とても", "3" to "めっちゃ", "4" to "相当","5" to "とんでもなく", "6" to "SUPER","7" to "DX", "8" to "お前だけは", "9" to "おそらく")
             val map2 = mapOf("0" to "大吉", "1" to "庄吉", "2" to "小吉", "3" to "茂吉", "4" to "中吉","5" to "大凶", "6" to "たぬきち","7" to "こっちみんな", "8" to "無吉", "9" to "ナイス！")
 
             val makeResult = map1[firstText] + map2[secondText]
-            resultText.text = makeResult
+
+//            println(firstText)
+
+            fortuneBtn.setOnClickListener {
+
+                resultText.text = makeResult
+
+
+
+            }
+
         }
+        getNumber()
+
+
+
+//        fortuneBtn.setOnClickListener {
+//            val firstText:String = firstNumber.text.toString()
+//            val secondText = secondNumber.text.toString()
+//
+//
+//            println(firstText)
+//
+//            val map1 = mapOf("0" to "なかなか", "1" to "まずまず", "2" to "とても", "3" to "めっちゃ", "4" to "相当","5" to "とんでもなく", "6" to "SUPER","7" to "DX", "8" to "お前だけは", "9" to "おそらく")
+//            val map2 = mapOf("0" to "大吉", "1" to "庄吉", "2" to "小吉", "3" to "茂吉", "4" to "中吉","5" to "大凶", "6" to "たぬきち","7" to "こっちみんな", "8" to "無吉", "9" to "ナイス！")
+//
+//            val makeResult = map1[firstText] + map2[secondText]
+//            resultText.text = makeResult
+//        }
     }
 
 }
